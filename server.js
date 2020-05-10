71017b2c7e3e4d45f02024741038ee46e66fe6e3
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-05-10 14:31:56 +0000

    Finish: read HTML file

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-let http = require('http');
+let http = require('http'),
+    fs = require('fs');
 let settings = require('./settings')
 let server = http.createServer();
 
 console.log(settings)
 
 server.on('request', function(req, res) {
-  switch (req.url) {
-    case '/about':
-      msg = 'about this page';
-      break;
-    case '/profile':
-      msg = 'about me';
-      break;
-    default:
-      msg = 'wrong page';
-      break;
-  }
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(msg);
-  res.end();
+  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('not found!');
+      return res.end();
+    }
+  res.writeHead(404, {'Content-Type': 'text/html'});
+  res.write(data);
+  return res.end();
+  });
 });
 server.listen(settings.port, settings.host);
 console.log('server listening ...');
