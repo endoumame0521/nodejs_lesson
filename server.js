9fe9792735b83176125a6ed5be86f152d8b17c19
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-05-10 15:06:57 +0000

    Finish: npm display form

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
-let http = require('http'),
-    fs = require('fs');
+let http = require('http');
+let fs = require('fs');
 let settings = require('./settings')
+let ejs = require('ejs')
+let qs = require('querystring')
+
 let server = http.createServer();
+let template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
+let posts = [];
+
+
+function renderForm(posts, res) {
+  let data = ejs.render(template, {
+    posts: posts
+  });
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(data);
+  return res.end();
+}
 
 console.log(settings)
 
 server.on('request', function(req, res) {
-  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.write('not found!');
-      return res.end();
-    }
-  res.writeHead(404, {'Content-Type': 'text/html'});
-  res.write(data);
-  return res.end();
-  });
+  if (req.method === 'POST') {
+    req.data = '';
+    req.on('readable', function() {
+      req.data += read.read();
+    });
+    req.on('end', function() {
+
+    });
+  } else {
+    renderForm(posts, res);
+  }
 });
 server.listen(settings.port, settings.host);
 console.log('server listening ...');
