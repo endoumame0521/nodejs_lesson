ad039ba96f2762b2b12126bb8d8b04700687dd03
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-05-10 14:22:57 +0000

    Finish: request url

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ let server = http.createServer();
 console.log(settings)
 
 server.on('request', function(req, res) {
+  switch (req.url) {
+    case '/about':
+      msg = 'about this page';
+      break;
+    case '/profile':
+      msg = 'about me';
+      break;
+    default:
+      msg = 'wrong page';
+      break;
+  }
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hello world');
+  res.write(msg);
   res.end();
 });
 server.listen(settings.port, settings.host);
